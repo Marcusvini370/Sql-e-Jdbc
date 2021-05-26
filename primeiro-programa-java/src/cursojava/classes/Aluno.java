ea63345df205aa0d63fe65db06cdc52f8de8fc71
Vincius <Vincius@DESKTOP-8FJ6FLG>
2021-05-25 23:43:33 -0300

    Alters

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -3,6 +3,8 @@ package cursojava.classes;
 import java.util.ArrayList;
 import java.util.List;
 
+import cursojava.constantes.StatusAluno;
+
 public class Aluno {
 	
 	/* Esses são os atributos do Aluno */
@@ -162,12 +164,12 @@ public class Aluno {
 			
 			if (media >= 50) {
 				if(media >= 70) {
-					return "Aluno está aprovado";
+					return StatusAluno.APROVADO;
 				}else {
-				return "Aluno está em recuperação";
+				return StatusAluno.RECUPERACAO;
 			}
 				}else {
-				return "Aluno está reprovado";
+				return StatusAluno.REPROVADO;
 			}
 			
 			}
